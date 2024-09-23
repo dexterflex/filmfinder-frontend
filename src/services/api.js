1546dfb50d8cb04c5beb0b707505c16077f1d17e
VIVEK KUMAR <vk281977@gmail.com>
2024-09-24 02:22:52 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://filmfinder-backend.vercel.app/api', // Change this if your backend is hosted elsewhere
+    baseURL: 'http://localhost:8000/api', // Change this if your backend is hosted elsewhere
     withCredentials: true // To handle cookies for authentication
 });
 
