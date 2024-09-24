1089c746319a24425a6f3477823b1c32b51b95fc
VIVEK KUMAR <vk281977@gmail.com>
2024-09-24 12:20:13 +0530

    Update MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,35 +1,37 @@
 import React from 'react';
-import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import movieServies from '../services/movieService'
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const MovieCard = ({ movie }) => {
     const navigate = useNavigate()
-    const [message, setMessage] = useState(null);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             await movieServies.addFavoriteMovie(movie);
-            setMessage("Added into Favourites")
+            toast(`${movie.title} is added to Favourites`)
         } catch (err) {
             if (err.response.status === 401) {
                 navigate('/signin')
             }
-            setMessage(err.response.data.message)
         }
     };
     return (
-        <div className="movie-card">
-            <Link to={`/movie/${movie.id}`}>
-                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-                <h3>{movie.title}</h3>
-                <p>Rating: {movie.vote_average.toFixed(1)} <i className="fa-solid fa-star" style={{ color: '#FFBF00' }}></i></p>
-                <p>{movie.release_date.split("-")[0]}</p>
-            </Link>
-            <p>{message}</p>
-            <button onClick={(e) => handleSubmit(e)}>Add to Favourites</button>
-        </div >
+        <>
+            <ToastContainer />
+            <div className="movie-card">
+                <Link to={`/movie/${movie.id}`}>
+                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                    <h3>{movie.title}</h3>
+                    <p>Rating: {movie.vote_average.toFixed(1)} <i className="fa-solid fa-star" style={{ color: '#FFBF00' }}></i></p>
+                    <p>{movie.release_date.split("-")[0]}</p>
+                </Link>
+                <button onClick={(e) => handleSubmit(e)}>Add to Favourites</button>
+            </div >
+        </>
     );
 };
 
